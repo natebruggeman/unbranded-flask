91f3fe7f828b5c8565f55f741ca42be2c6813cdb
Nate Bruggeman <njbruggeman@gmail.com>
2019-12-05 21:24:33 -0600

    create order route started

--- testget.py
+++ testget.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, jsonify
+from flask import Flask, render_template, g, jsonify, request
 import requests, json #connection to S&S
 from peewee import *
 
@@ -18,6 +18,18 @@ class Garment(Model):
 	gtin = IntegerField()
 	size = CharField()
 	price  = DecimalField(max_digits=5, decimal_places=2)
+
+	class Meta:
+		database = DATABASE
+
+class Order(Model):
+	items= CharField()
+	shippingAddress= CharField()
+	shippingCity= CharField()
+	shippingState= CharField()
+	shippingZip= IntegerField()
+	creditCard= IntegerField()
+	user = CharField()
 
 	class Meta:
 		database = DATABASE
@@ -74,6 +86,51 @@ def index():
 
 
 	return render_template('garments.html')
+
+
+
+
+
+
+
+@app.route('/orders', methods=['POST'])
+def create_orders():
+	"""
+	{
+	   "items":[
+	      {
+	         "gtin":884913636842,
+	         "qty":3
+	      },
+	      {
+	         "gtin":884913636866,
+	         "qty":5
+	      }
+	   ],
+	   "shippingAddress":"123 street",
+	   "shippingCity":"chicago",
+	   "shippingState":"IL",
+	   "shippingZip":"60647",
+	   "creditCard":"12345"
+	}
+	"""
+
+	data = request.get_json()
+	print(data)
+
+	order = Order(items=data["items"],
+		shippingAddress= data["shippingAddress"],
+		shippingCity= data["shippingCity"],
+		shippingState= data["shippingState"],
+		shippingZip= data["shippingZip"],
+		creditCard= data["creditCard"],
+		user = "this will be from session"
+		)
+	order.save()
+
+	return 200
+
+
 
 # req = requests.get(url='https://api.ssactivewear.com/v2/products/?style=00760,Gildan 5000', 
 # 	auth=('504411', '4f4d8aa6-3d5b-4b63-a200-148ebc775938'))
